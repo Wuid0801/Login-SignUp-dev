c074efde2010b77eaf76b1e7b0742739af50f618
Wuid0801 <pig10326@naver.com>
2024-03-18 23:02:22 +0900

    [FEAT] shadcn/ui 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>계정을 생성합니다</CardTitle>
+                    <CardDescription>필수 정보를 입력해볼게요</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">이름</Label>
+                                <Input id="name" placeholder="홍길동" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">이메일</Label>
+                                <Input id="name" placeholder="hello@sparta-devcamp.com" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">연락처</Label>
+                                <Input id="name" placeholder="01000000000" />
+                            </div>
+                            
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="framework">역할</Label>
+                                <Select>
+                                    <SelectTrigger id="framework">
+                                        <SelectValue placeholder="역할을 선택해주세요" />
+                                    </SelectTrigger>
+                                    <SelectContent position="popper">
+                                        <SelectItem value="관리자">관리자</SelectItem>
+                                        <SelectItem value="일반사용자">일반사용자</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex">
+                    <Button>다음 단계로</Button>
+                </CardFooter>
+            </Card>
+        </>
+    );
 }
 
-export default App
+export default App;
